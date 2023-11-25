ef9e5ea801eb074fe0c52d5b734f1505d24529e4
Mitesh Khemani <miteshkhemani8@gmail.com>
2023-11-25 23:23:48 +0530

    added average economy and bowling strike rate against individual teams graphs

--- src/graph_generation/graph.py
+++ src/graph_generation/graph.py
@@ -1,11 +1,11 @@
 import json
 import os
 import plotly.express as px
-import plotly.subplots as sp
 import plotly.graph_objects as go
 import pandas as pd
 from collections import defaultdict
 
+# terminolofy mapping for extracting data from json files
 terminology_mapping = {
     "tt": "type",
     "sp": "span",
@@ -30,6 +30,7 @@ terminology_mapping = {
     "ov": "overs_bowled"
 }
 
+# function for batting strike rate vs batting position graph
 def generate_batting_strike_rate_vs_batting_position_graph(data, output_directory, player_name):
     batting_positions = []
     batting_strike_rates = []
@@ -52,10 +53,11 @@ def generate_batting_strike_rate_vs_batting_position_graph(data, output_director
     df["Batting Position"] = pd.Categorical(df["Batting Position"], categories=custom_order, ordered=True)
     df = df.sort_values("Batting Position")
 
-    fig = px.bar(df, x="Batting Position", y="Batting Strike Rate", title=f"{player_title}'s Batting Strike Rate at Batting Position")
+    fig = px.bar(df, x="Batting Position", y="Batting Strike Rate", color="Batting Strike Rate", title=f"{player_title}'s Batting Strike Rate at Batting Position")
     image_file_path = os.path.join(output_directory, f"{player_name}_strike_rate.png")
     fig.write_image(image_file_path)
 
+# function for runs scored vs batting position graph
 def generate_runs_scored_vs_batting_position_graph(data, output_directory, player_name):
     batting_positions = []
     runs_scored = []
@@ -78,10 +80,11 @@ def generate_runs_scored_vs_batting_position_graph(data, output_directory, playe
     df["Batting Position"] = pd.Categorical(df["Batting Position"], categories=custom_order, ordered=True)
     df = df.sort_values("Batting Position")
 
-    fig = px.bar(df, x="Batting Position", y="Runs Scored", title=f"{player_title}'s Runs Scored at Batting Position")
+    fig = px.bar(df, x="Batting Position", y="Runs Scored", color="Runs Scored", title=f"{player_title}'s Runs Scored at Batting Position")
     image_file_path = os.path.join(output_directory, f"{player_name}_runs_scored.png")
     fig.write_image(image_file_path)
 
+# function for generating not out percentages
 def generate_not_out_percentages_graph(player_data, output_directory, player_name):
     odi_matches = player_data.get("odi_matches", [])
     not_out_counts = defaultdict(int)
@@ -110,6 +113,7 @@ def generate_not_out_percentages_graph(player_data, output_directory, player_nam
     image_file_path_not_out_percentages = os.path.join(output_directory, f"{player_name}_not_out_percentages.png")
     fig.write_image(image_file_path_not_out_percentages)
 
+# function for generating batting strike rate graph
 def generate_batting_strike_rate_graph(data, output_directory, player_name):
     years = []
     batting_strike_rates = []
@@ -136,6 +140,7 @@ def generate_batting_strike_rate_graph(data, output_directory, player_name):
     image_file_path_strike_rate = os.path.join(output_directory, f"{player_name}_strike_rate.png")
     fig_strike_rate.write_image(image_file_path_strike_rate)
 
+# function for generating batting average graph
 def generate_batting_average_graph(data, output_directory, player_name):
     years = []
     batting_averages = []
@@ -162,6 +167,7 @@ def generate_batting_average_graph(data, output_directory, player_name):
     image_file_path_average = os.path.join(output_directory, f"{player_name}_average.png")
     fig_average.write_image(image_file_path_average)
 
+# function for generating runs scored graph
 def generate_runs_scored_graph(data, output_directory, player_name):
     years = []
     runs_scored = []
@@ -182,12 +188,13 @@ def generate_runs_scored_graph(data, output_directory, player_name):
         "Runs Scored": runs_scored
     })
 
-    fig_runs = px.line(df_runs, x="Year", y="Runs Scored", labels={"x": "Year", "y": "Runs Scored"})
+    fig_runs = px.scatter(df_runs, x="Year", y="Runs Scored", size="Runs Scored", color="Runs Scored", labels={"x": "Year", "y": "Runs Scored"})
     fig_runs.update_layout(title=f"{player_title}'s Runs Scored Over the Years (2017 and Beyond)")
     fig_runs.update_xaxes(tickvals=years)
     image_file_path_runs = os.path.join(output_directory, f"{player_name}_runs.png")
     fig_runs.write_image(image_file_path_runs)
 
+# functions for generating fifties vs hundreds graph
 def generate_fifties_vs_hundreds_graph(data, output_directory, player_name):
     years = []
     fifties = []
@@ -218,7 +225,7 @@ def generate_fifties_vs_hundreds_graph(data, output_directory, player_name):
     image_file_path_fifties_vs_hundreds = os.path.join(output_directory, f"{player_name}_fifties_vs_hundreds.png")
     fig_fifties_vs_hundreds.write_image(image_file_path_fifties_vs_hundreds)
 
-
+# function for generating wickets taken graph
 def generate_wickets_taken_graph(data, output_directory, player_name):
     years = []
     wickets_taken = []
@@ -240,7 +247,7 @@ def generate_wickets_taken_graph(data, output_directory, player_name):
         "Wickets Taken": wickets_taken
     })
 
-    fig_wickets = px.line(df_wickets, x="Year", y="Wickets Taken", labels={"x": "Year", "y": "Wickets Taken"})
+    fig_wickets = px.bar(df_wickets, x="Year", y="Wickets Taken", color="Wickets Taken", labels={"x": "Year", "y": "Wickets Taken"})
     fig_wickets.update_layout(title=f"{player_title}'s Wickets Taken Over the Years (2017 and Beyond)")
     fig_wickets.update_xaxes(title_text=None)
     fig_wickets.update_xaxes(tickvals=years)  
@@ -255,6 +262,7 @@ def generate_wickets_taken_graph(data, output_directory, player_name):
     image_file_path_wickets = os.path.join(output_directory, f"{player_name}_wickets.png")
     fig_wickets.write_image(image_file_path_wickets)
 
+# function for generating average economy graph
 def generate_average_economy_graph(data, output_directory, player_name):
     years = []
     average_economy = []
@@ -291,6 +299,7 @@ def generate_average_economy_graph(data, output_directory, player_name):
     image_file_path_average_economy = os.path.join(output_directory, f"{player_name}_average_economy.png")
     fig_average_economy.write_image(image_file_path_average_economy)
 
+# function for generating maiden overs graph
 def generate_maiden_overs_graph(data, output_directory, player_name):
     years = []
     maiden_overs = []
@@ -327,6 +336,7 @@ def generate_maiden_overs_graph(data, output_directory, player_name):
     image_file_path_maiden_overs = os.path.join(output_directory, f"{player_name}_maiden_overs.png")
     fig_maiden_overs.write_image(image_file_path_maiden_overs)
 
+# function for generating bowling strike rate graph
 def generate_bowling_strike_rate_graph(data, output_directory, player_name):
     years = []
     bowling_strike_rates = []
@@ -363,6 +373,7 @@ def generate_bowling_strike_rate_graph(data, output_directory, player_name):
     image_file_path_bowling_strike_rate = os.path.join(output_directory, f"{player_name}_bowling_strike_rate.png")
     fig_bowling_strike_rate.write_image(image_file_path_bowling_strike_rate)
 
+# function for generating overs bowled graph
 def generate_overs_bowled_graph(data, output_directory, player_name):
     years = []
     total_overs_bowled = []
@@ -379,26 +390,13 @@ def generate_overs_bowled_graph(data, output_directory, player_name):
                         total_overs_bowled.append(float(year_data["overs_bowled"]))
 
     player_title = player_name.replace('_', ' ').title()
-    df_overs_bowled = pd.DataFrame({
-        "Year": years,
-        "Total Overs Bowled": total_overs_bowled
-    })
-
-    fig_overs_bowled = px.line(df_overs_bowled, x="Year", y="Total Overs Bowled", labels={"x": "Year", "y": "Total Overs Bowled"})
+    labels = [f"{year} ({overs} overs)" for year, overs in zip(years, total_overs_bowled)]
+    fig_overs_bowled = go.Figure(data=[go.Pie(labels=labels, values=total_overs_bowled)])
     fig_overs_bowled.update_layout(title=f"{player_title}'s Total Overs Bowled Over the Years (2017 and Beyond)")
-    fig_overs_bowled.update_xaxes(title_text=None)
-    fig_overs_bowled.update_xaxes(tickvals=years)  
-    fig_overs_bowled.add_annotation(
-        text="Year",
-        x=0.5,
-        y=-0.1,
-        xref="paper",
-        yref="paper",
-        showarrow=False
-    )
     image_file_path_overs_bowled = os.path.join(output_directory, f"{player_name}_overs_bowled.png")
     fig_overs_bowled.write_image(image_file_path_overs_bowled)
 
+# function for generating runs scored against individual teams graph
 def generate_runs_scored_against_individual_teams_graph(player_data, output_directory, player_name):
     for stats_group in data["BATTING"]["summary"]["groups"]:
         if stats_group["type"] == "OPPOSITION_TEAM":
@@ -415,13 +413,14 @@ def generate_runs_scored_against_individual_teams_graph(player_data, output_dire
             })
 
     df = pd.DataFrame(runs_data)
-    fig = px.bar(df, x="Opposition", y="Runs Scored", labels={"x": "Opposition Team", "y": "Runs Scored"})
+    fig = px.bar(df, x="Opposition", y="Runs Scored", color="Runs Scored", labels={"x": "Opposition Team", "y": "Runs Scored"})
     
     player_title = player_name.replace('_', ' ').title()
     fig.update_layout(title=f"{player_title}'s Runs Scored Against Individial Teams")
     image_file_path_runs_scored_against_individual_teams = os.path.join(output_directory, f"{player_name}_runs_scored_against_individual_teams.png")
     fig.write_image(image_file_path_runs_scored_against_individual_teams)
 
+# function for generating batting strike rate against individual teams 
 def generate_batting_strike_rate_against_individual_teams_graph(player_data, output_directory, player_name):
     for stats_group in data["BATTING"]["summary"]["groups"]:
         if stats_group["type"] == "OPPOSITION_TEAM":
@@ -438,14 +437,115 @@ def generate_batting_strike_rate_against_individual_teams_graph(player_data, out
             })
 
     df = pd.DataFrame(runs_data)
-    fig = px.bar(df, x="Opposition", y="Batting Strike Rate", labels={"x": "Opposition Team", "y": "Batting Strike Rate"})
+    fig = px.bar(df, x="Opposition", y="Batting Strike Rate",color="Batting Strike Rate", labels={"x": "Opposition Team", "y": "Batting Strike Rate"})
     
     player_title = player_name.replace('_', ' ').title()
     fig.update_layout(title=f"{player_title}'s Batting Strike Rate Against Individial Teams")
     image_file_path_batting_strike_rate_against_individual_teams = os.path.join(output_directory, f"{player_name}_batting_strike_rate_against_individual_teams.png")
     fig.write_image(image_file_path_batting_strike_rate_against_individual_teams)
 
+# function for generating not our percentages against individual teams graph
+def generate_not_out_percentages_against_individual_team_graph(player_data, output_directory, player_name):
+    for stats_group in data["BATTING"]["summary"]["groups"]:
+        if stats_group["type"] == "OPPOSITION_TEAM":
+            opposition_data = stats_group.get("stats", [])
+            break
 
+    runs_data = []
+    for opposition in opposition_data:
+        opposition_name = opposition.get("tt", "")
+        if(opposition_name in ["vs Australia", "vs England", "vs Pakistan", "vs New Zealand", "vs Sri Lanka", "vs Bangladesh", "vs West Indies", "vs Afghanistan"]):
+            runs_data.append({
+                "Opposition": opposition_name,
+                "Not Outs": opposition.get("no", 0),
+                "Total Matches": opposition.get("mt", 0),
+            })
+
+    df = pd.DataFrame(runs_data)
+    df["Not Out Percentages"] = (df["Not Outs"] / df["Total Matches"]) * 100 
+    player_title = player_name.replace('_', ' ').title()
+    fig = px.pie(df, names="Opposition", values="Not Out Percentages", title=f"{player_title}'s Not Out Percentages Against Individual Teams")
+    image_file_path_batting_strike_rate_against_individual_teams = os.path.join(output_directory, f"{player_name}_not_out_percentages_against_individual_teams.png")
+    fig.write_image(image_file_path_batting_strike_rate_against_individual_teams)
+
+# function for generating wickets taken against individuyal teams graph
+def generate_wickets_taken_against_individual_teams_graph(player_data, output_directory, player_name):
+    opposition_data = None
+
+    for stats_group in player_data["BOWLING"]["summary"]["groups"]:
+        if stats_group["type"] == "OPPOSITION_TEAM":
+            opposition_data = stats_group.get("stats", [])
+            break
+
+    runs_data = []
+    for opposition in opposition_data:
+        opposition_name = opposition.get("tt", "")
+        if opposition_name in ["vs Australia", "vs England", "vs Pakistan", "vs New Zealand", "vs Sri Lanka", "vs Bangladesh", "vs West Indies", "vs Afghanistan"]:
+            runs_data.append({
+                "Opposition": opposition_name,
+                "Wickets": opposition.get("wk", 0),
+            })
+
+    df = pd.DataFrame(runs_data)
+    df = df.dropna(subset=["Wickets"])
+    
+    player_title = player_name.replace('_', ' ').title()
+    fig = px.scatter(df, x="Opposition", y="Wickets", size="Wickets", color="Wickets", title=f"{player_title}'s Wickets Against Individual Teams")
+    image_file_path_wickets_taken_against_individual_teams = os.path.join(output_directory, f"{player_name}_wickets_taken_against_individual_teams.png")
+    fig.write_image(image_file_path_wickets_taken_against_individual_teams)
+
+# functions for generating average economy against individual teams graph
+def generate_average_economy_against_individual_teams_graph(player_data, output_directory, player_name):
+    opposition_data = None
+    for stats_group in player_data["BOWLING"]["summary"]["groups"]:
+        if stats_group["type"] == "OPPOSITION_TEAM":
+            opposition_data = stats_group.get("stats", [])
+            break
+
+    bowling_economy_data = []
+    for opposition in opposition_data:
+        opposition_name = opposition.get("tt", "")
+        if opposition_name in ["vs Australia", "vs England", "vs Pakistan", "vs New Zealand", "vs Sri Lanka", "vs Bangladesh", "vs West Indies", "vs Afghanistan"]:
+            bowling_economy_data.append({
+                "Opposition": opposition_name,
+                "Average Economy": opposition.get("bwe", 0),
+            })
+
+    df = pd.DataFrame(bowling_economy_data)
+
+    # Drop rows with NaN values in the "Average Economy" column
+    df = df.dropna(subset=["Average Economy"])
+    player_title = player_name.replace('_', ' ').title()
+    fig = px.bar(df, x="Opposition", y="Average Economy", color="Average Economy", title=f"{player_title}'s Average Economy Against Individual Teams")
+    image_file_path_average_economy_against_individual_teams = os.path.join(output_directory, f"{player_name}_average_economy_against_individual_teams.png")
+    fig.write_image(image_file_path_average_economy_against_individual_teams)
+
+# function for generating bowling strike rate against individual teams graph
+def generate_bowling_strike_rate_against_individual_teams_graph(player_data, output_directory, player_name):
+    opposition_data = None
+
+    for stats_group in player_data["BOWLING"]["summary"]["groups"]:
+        if stats_group["type"] == "OPPOSITION_TEAM":
+            opposition_data = stats_group.get("stats", [])
+            break
+
+    bowling_strike_rate = []
+    for opposition in opposition_data:
+        opposition_name = opposition.get("tt", "")
+        if opposition_name in ["vs Australia", "vs England", "vs Pakistan", "vs New Zealand", "vs Sri Lanka", "vs Bangladesh", "vs West Indies", "vs Afghanistan"]:
+            bowling_strike_rate.append({
+                "Opposition": opposition_name,
+                "Bowling Strike Rate": opposition.get("bwsr", 0),
+            })
+
+    df = pd.DataFrame(bowling_strike_rate)
+
+    # Drop rows with NaN values in the "Bowling Strike Rate" column
+    df = df.dropna(subset=["Bowling Strike Rate"])
+    player_title = player_name.replace('_', ' ').title()
+    fig = px.bar(df, x="Opposition", y="Bowling Strike Rate", color="Bowling Strike Rate", title=f"{player_title}'s Bowling Strike Rate Against Individual Teams")
+    image_file_path_bowling_strike_rate_against_individual_teams = os.path.join(output_directory, f"{player_name}_bowling_strike_rate_against_individual_teams.png")
+    fig.write_image(image_file_path_bowling_strike_rate_against_individual_teams)
 
 output_directory_strike_rates = os.path.abspath("./Overall_Player_Stats/Batting_Stats/Batting_Strike_Rates")
 output_directory_averages = os.path.abspath("./Overall_Player_Stats/Batting_Stats/Batting_Averages")
@@ -461,6 +561,10 @@ output_directory_bowling_strike_rate = os.path.abspath("./Overall_Player_Stats/B
 output_directory_overs_bowled = os.path.abspath("./Overall_Player_Stats/Bowling_Stats/Overs_Bowled")
 output_directory_runs_scored_against_individual_teams = os.path.abspath("./Vs_Other_Teams/Runs_Scored_Against_Individual_Teams")
 output_directory_batting_strike_rate_against_individual_teams = os.path.abspath("./Vs_Other_Teams/Batting_Strike_Rate_Against_Individual_Teams")
+output_directory_not_out_percentages_againt_individual_teams = os.path.abspath("./Vs_Other_Teams/Not_Out_Percentages_Against_Individual_Teams")
+output_directory_wickets_taken_against_individual_teams = os.path.abspath("./Vs_Other_Teams/Wickets_Taken_Against_Individual_Teams")
+output_directory_average_economy_against_individual_teams = os.path.abspath("./Vs_Other_Teams/Average_Economy_Against_Individual_Teams")
+output_directory_bowling_strike_rate_against_individual_teams = os.path.abspath("./Vs_Other_Teams/Bowling_Strike_Rate_Against_Individual_Teams")
 
 os.makedirs(output_directory_strike_rates, exist_ok=True)
 os.makedirs(output_directory_averages, exist_ok=True)
@@ -476,7 +580,10 @@ os.makedirs(output_directory_bowling_strike_rate, exist_ok=True)
 os.makedirs(output_directory_overs_bowled, exist_ok=True)
 os.makedirs(output_directory_runs_scored_against_individual_teams, exist_ok=True)
 os.makedirs(output_directory_batting_strike_rate_against_individual_teams, exist_ok=True)
-
+os.makedirs(output_directory_not_out_percentages_againt_individual_teams, exist_ok=True)
+os.makedirs(output_directory_wickets_taken_against_individual_teams, exist_ok=True)
+os.makedirs(output_directory_average_economy_against_individual_teams, exist_ok=True)
+os.makedirs(output_directory_bowling_strike_rate_against_individual_teams, exist_ok=True)
 
 json_file_path = os.path.abspath("../../data/players.json")
 
@@ -506,5 +613,9 @@ for player_name in players:
     generate_overs_bowled_graph(data, output_directory_overs_bowled, player_name)
     generate_runs_scored_against_individual_teams_graph(data, output_directory_runs_scored_against_individual_teams, player_name)
     generate_batting_strike_rate_against_individual_teams_graph(data, output_directory_batting_strike_rate_against_individual_teams, player_name)
+    generate_not_out_percentages_against_individual_team_graph(data, output_directory_not_out_percentages_againt_individual_teams, player_name)
+    generate_wickets_taken_against_individual_teams_graph(data, output_directory_wickets_taken_against_individual_teams, player_name)
+    generate_average_economy_against_individual_teams_graph(data, output_directory_average_economy_against_individual_teams, player_name)
+    generate_bowling_strike_rate_against_individual_teams_graph(data, output_directory_bowling_strike_rate_against_individual_teams, player_name)
 
 print("Graphs saved in their respective folders.")
